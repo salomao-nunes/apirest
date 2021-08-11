fdc085bd9c1deacd7da8e3217b9ab0cac5c6c8c1
Donald Timoteo <donaldtimoteo@Donalds-MacBook-Pro.local>
2021-08-11 17:21:13 +0100

    Initial release

---
+++ app/Http/Controllers/api/ProductController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Http\Requests;
+use App\Http\Resources\Product as ProductResource;
+use Validator;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $product = Product::all();
+
+        return  new ProductResource($product);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $product = new Product();
+        $product->name= $request->input('name');
+        $product->price= $request->input('price');
+        
+        $product->save();
+
+        return new ProductResource($product);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+       return Product::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+
+        $product= Product::find($id);
+        $product->update($request->all());
+
+        return $product;
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        return Product::destroy($id);
+    }
+
+
+    public function testData(Request $req){
+
+             
+        return ["x"=>"y"];
+            
+
+
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\api\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/products',[ProductController::class,'index']);
+Route::get('/product/{id}',[ProductController::class,'show']);
+Route::post('/products',[ProductController::class,'store']);
+Route::put('/product/{id}',[ProductController::class,'update']);
+Route::delete('/product/{id}',[ProductController::class,'destroy']);
+
+//Validation
+Route::post('/save',[ProductController::class,'testData']);
